c34a88d3db2298fad755d2813b84ce95ab0ac653
Sean Doyle <sdoylelambda@gmail.com>
2023-02-17 14:16:44 -0500

    Login and create users forms, routes, and model.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, redirect, url_for, render_template, request
-from models import db
+from flask import Flask, redirect, url_for, render_template, request, flash
+from models import db, login_manager, User, ToDO
+from forms import RegisterForm, LoginForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user
 
 # Flask
 app = Flask(__name__)
@@ -24,12 +27,41 @@ def index():
     return "Hello World"
 
 
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+    if request.method == 'GET':
+        return render_template('register.html', form=form)
+
+    if request.method == 'POST':
+        if form.validate_on_submit:
+            user = User(email=form.username.data,
+                        password=generate_password_hash(form.password.data)
+                        )
+            db.session.add(user)
+            db.session.commit()
+            return redirect('/login')
+
+
 @app.route("/login")
 def login():
     '''
     Home page - Sign up/Sign in
     '''
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit:
+        user = User.query.filter_by(email=form.username.data)
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect('/todos')
+        flash("Invalid details")
+
+    return render_template('login.html', form=form)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 @app.route("/todo")
--- models.py
+++ models.py
@@ -1,16 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(20))
+    # todos = db.relationship('TodoItem', backref='user')
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -21,6 +26,10 @@ class ToDO(db.Model):
     title = db.Column(db.String(1000))
     due_date = db.Column(db.String(10))
     complete = db.Column(db.Boolean)
+    todo_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'Task{self.ToDo.title}{self.due_date}'
 
 
 with app.app_context():
